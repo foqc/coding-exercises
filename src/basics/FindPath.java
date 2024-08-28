78965116681e4412535875ce53c202d6a6c58350
foqc <fabianq44@gmail.com>
2024-08-27 19:38:02 -0500

    feature: find path in maze with dinamic programming

---
+++ src/basics/FindPath.java
@@ -0,0 +1,63 @@
+package basics;
+
+import java.util.Arrays;
+
+public class FindPath {
+
+  /*
+   * Mini maze
+   * Given m * n matrix find path
+   * The letter I is the beginning where you must begin to traverse the matrix.
+   * Number 1 is walled and cannot pass through there.
+   * The number 0 is a path, it means that you can walk there.
+   */
+
+  public static boolean canMove(Character[][] table, boolean[][] visited, int i, int j) {
+    return (i >= 0 && i < table.length && j >= 0 && j < table.length
+        && !visited[i][j] && (table[i][j] == '0' || table[i][j] == 'I' || table[i][j] == 'S'));
+  }
+
+  /*
+   * m=rows length, n=cols length
+   * Time complexity: O(2^n*m)
+   * Space complexity: O(m*n)
+   */
+  public static boolean move(Character[][] table, boolean[][] visited, int i, int j) {
+    if (i >= 0 && i < table.length && j >= 0 && j < table.length
+        && table[i][j] == 'S') {
+      visited[i][j] = true;
+      return true;
+    }
+    if (canMove(table, visited, i, j)) {
+      visited[i][j] = true;
+
+      if (move(table, visited, i + 1, j)) {
+        return true;
+      }
+      if (move(table, visited, i, j + 1)) {
+        return true;
+      }
+      if (move(table, visited, i - 1, j)) {
+        return true;
+      }
+      return move(table, visited, i, j - 1);
+    }
+
+    return false;
+  }
+
+
+  public static void main(String... args) {
+    Character[][] matrix = {
+        {'I', '0', '0', '1', 'S'},
+        {'1', '0', '1', '1', '0'},
+        {'1', '0', '0', '1', '0'},
+        {'0', '0', '1', '0', '0'},
+        {'1', '0', '0', '0', '0'}};
+
+    boolean[][] visited = new boolean[matrix.length][matrix[0].length];//by default the matrix has false
+    System.out.println(
+        "Can Move? " + move(matrix, visited, 0, 0) + " R: " + Arrays.deepToString(visited));
+  }
+
+}
